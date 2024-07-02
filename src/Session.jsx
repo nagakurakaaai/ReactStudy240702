351bc0b3d05891c371e7c4a2b4abd666e08f41e2
nagakurakai <nagakura@mediowl.co.jp>
2024-07-02 17:38:41 +0900

    sessionファイルの一部を共通コンポーネント化

--- src/Session.jsx
+++ src/Session.jsx
@@ -1,3 +1,4 @@
+import SessionArticle from './SessionArticle.jsx'
 
 function Session () {
     return(
@@ -11,34 +12,10 @@ function Session () {
                         <div className="session__schedule">
                             <table className="table-type02">
                                 <tbody>
-                                    <tr>
-                                        <th className="session-table__time">13:00-<br className="sp" />14:00</th>
-                                        <td>
-                                            <h3 className="session-table__title">ライトニングトーク：定時に帰る、それはアンドロイドになること</h3>
-                                            <p>スピーカー：ショーン・デイビット・ジュニア<br/>人間らしくあろうとすること、それが残業を生むのだ。さぁ！アンドロイドになるのだ！</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th className="session-table__time">14:00-<br className="sp" />18:00</th>
-                                        <td>
-                                            <h3 className="session-table__title">ショーンのとっておきの定時に帰る方法</h3>
-                                            <p>スピーカー：ショーン・デイビット・ジュニア<br/>アンドロイド的定時に帰る方法を紹介。ワタシ、テイジデカエリマス。</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th className="session-table__time">18:00-<br className="sp" />24:00</th>
-                                        <td>
-                                            <h3 className="session-table__title">公私混同仕事術</h3>
-                                            <p>スピーカー：ミホコ・テニース<br/>私は会社にもテニスラケットを持ち込む。そう、もういっそのこと仕事と趣味の境界線を消しましょう。</p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th className="session-table__time">24:00-<br className="sp" />13:00</th>
-                                        <td>
-                                            <h3 className="session-table__title">超ネット仕事術</h3>
-                                            <p>スピーカー：ジェシカ・パンナコッタ<br/>７年間一度も外出せずに仕事をする方法を紹介。自宅大好きっ！</p>
-                                        </td>
-                                    </tr>
+                                    <SessionArticle fromTime="13:00-" toTime="14:00" title="ライトニングトーク：定時に帰る、それはアンドロイドになること" text="スピーカー：ショーン・デイビット・ジュニア<br/>人間らしくあろうとすること、それが残業を生むのだ。さぁ！アンドロイドになるのだ！" />
+                                    <SessionArticle fromTime="14:00-" toTime="18:00" title="ショーンのとっておきの定時に帰る方法" text="スピーカー：ショーン・デイビット・ジュニア<br/>アンドロイド的定時に帰る方法を紹介。ワタシ、テイジデカエリマス。" />
+                                    <SessionArticle fromTime="18:00-" toTime="24:00" title="公私混同仕事術" text="スピーカー：ミホコ・テニース<br/>私は会社にもテニスラケットを持ち込む。そう、もういっそのこと仕事と趣味の境界線を消しましょう。" />
+                                    <SessionArticle fromTime="24:00-" toTime="13:00" title="超ネット仕事術" text="スピーカー：ジェシカ・パンナコッタ<br/>７年間一度も外出せずに仕事をする方法を紹介。自宅大好きっ！" />
                                 </tbody>
                             </table>
                         </div>
---
+++ src/SessionArticle.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+function SessionArticle (props) {
+    const {fromTime, toTime, title, text} = props
+    return(
+        <>
+            <tr>
+                <th className="session-table__time">{fromTime}<br className="sp" />{toTime}</th>
+                <td>
+                    <h3 className="session-table__title">{title}</h3>
+                    <p>{text}</p>
+                </td>
+            </tr>
+        </>
+    )
+}
+
+SessionArticle.propTypes = {
+    fromTime: PropTypes.string,
+    toTime: PropTypes.string,
+    title: PropTypes.string,
+    text: PropTypes.string,
+  };
+
+export default SessionArticle
