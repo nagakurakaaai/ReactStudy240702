ac96089ba4749dbef018b755dd7a2866bc220222
nagakurakai <nagakura@mediowl.co.jp>
2024-07-03 13:50:24 +0900

    <br/>が文字列として表示される問題の修正

--- src/SessionArticle.jsx
+++ src/SessionArticle.jsx
@@ -1,14 +1,23 @@
+import React from 'react'
 import PropTypes from 'prop-types';
 
 function SessionArticle (props) {
     const {fromTime, toTime, title, text} = props
+
+    // <br/>で改行されるようにtextを整形
+    const textSplit = text.split("<br/>").map((item, index) => {
+        return (
+            <React.Fragment key={index}>{item}<br /></React.Fragment>
+        );
+    })
+
     return(
         <>
             <tr>
                 <th className="session-table__time">{fromTime}<br className="sp" />{toTime}</th>
                 <td>
                     <h3 className="session-table__title">{title}</h3>
-                    <p>{text}</p>
+                    <p>{textSplit}</p>
                 </td>
             </tr>
         </>
