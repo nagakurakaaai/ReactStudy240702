25af04ddf39abfdcf296722e08e40ce82b0ff761
nagakurakai <nagakura@mediowl.co.jp>
2024-07-04 14:02:06 +0900

    commonフォルダ追加、ファイル移動

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Header from './Header.jsx'
+import Header from './common/Header.jsx'
 import KeyVisual from './KeyVisual.jsx'
 import About from './About.jsx'
 import Speaker from './Speaker.jsx'
 import Session from './Session.jsx'
 import Resistor from './Resistor.jsx'
 import Hooter from './Hooter.jsx'
-import UpButton from './UpButton.jsx'
+import UpButton from './common/UpButton.jsx'
 
 function App() {
   return (
